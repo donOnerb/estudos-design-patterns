a1eeb8b0be1e877ad483955ac2dad90643727838
Breno Vinicius de Oliveira <breno.oliveira@opus-software.com.br>
2021-03-23 17:36:37 -0300

    Adicionado exemplo para o padrão de projeto Adapter

---
+++ src/ProvedorDeDados.java
@@ -0,0 +1,12 @@
+public class ProvedorDeDados {
+    private UtilitariosProvedorDeDados utilitariosProvedorDeDados;
+
+
+    public ProvedorDeDados(UtilitariosProvedorDeDados utilitariosProvedorDeDados) {
+        this.utilitariosProvedorDeDados = utilitariosProvedorDeDados;
+    }
+    public String plotData(String data) {
+        var A = new A(data + "Sou o Provedor de dados, eu transformei o dado em A\n");
+        return utilitariosProvedorDeDados.plotData(A);
+    }
+}
